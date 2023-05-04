d7ff3d51af855de7b5115de2a0195bc39a97aa94
Martin Haas <martinhaas2005@gmail.com>
2023-05-04 15:46:28 -0300

    added complete page|not responsive/repetitive code/no smooth animations

--- js/navConfig.js
+++ js/navConfig.js
@@ -1,12 +1,21 @@
 const $navText = document.querySelectorAll('.nav-text');
+const $sections = document.querySelectorAll('section');
 
 $navText[1].classList.add('nav-active');
 
 let oldIndex = 1;
+
 $navText.forEach(function(e, index){
     e.addEventListener('click', () =>{
-        $navText[oldIndex].classList.remove('nav-active');;
-        $navText[index].classList.add('nav-active');
-        oldIndex = index;
+        if(index != oldIndex){
+            $navText[oldIndex].classList.remove('nav-active');;
+            $navText[index].classList.add('nav-active');
+    
+            $sections[oldIndex].setAttribute('data-status', 'inactive');
+            $sections[index].setAttribute('data-status', 'active');
+    
+            oldIndex = index;
+        }        
     })
 })
+
